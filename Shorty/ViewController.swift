3ea03092fbcdb4875b551794b94f79aa6d3584b5
avi <avinashsajjan@gmail.com>
2015-02-09 23:16:21 +0530

    modified view with buttons and actions to create short url and copy. A method which actually does the shortening of URL.

--- Shorty/ViewController.swift
+++ Shorty/ViewController.swift
@@ -8,8 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var urlField: UITextField!
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var shortenButton: UIBarButtonItem!
+    @IBOutlet weak var shortLabel: UIBarButtonItem!
+    @IBOutlet weak var clipboardButton: UIBarButtonItem!
+    
+    let goDaddyAccountKey = "012345"
+    var shortenURLConnection: NSURLConnection?
+    var shortURLData: NSMutableData?
+    
+    @IBAction func loadLocation(AnyObject) {
+        var urlText = urlField.text
+        
+        if !urlText.hasPrefix("http:") && !urlText.hasSuffix("https:") {
+            if !urlText.hasPrefix("//") {
+                urlText = "//" + urlText
+            }
+            urlText = "http:" + urlText
+        }
+        
+        if let url = NSURL(string: urlText) {
+            println(url)
+            webView.loadRequest(NSURLRequest(URL: url))
+        }
+
+    }
+    
+    
+    @IBAction func shortenURL(sender: AnyObject) {
+        if let toShorten = webView.request?.URL.absoluteString {
+            if let encodeURL = toShorten.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+                let urlString = "http://api.x.co/Squeeze.svc/text/\(goDaddyAccountKey)?url=\(encodeURL)"
+                shortURLData = NSMutableData()
+                let request = NSURLRequest(URL: NSURL(string: urlString)!)
+                shortenURLConnection = NSURLConnection(request: request, delegate: self)
+                shortenButton.enabled = false
+            }
+        }
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        shortenButton.enabled = false
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        shortenButton.enabled = true
+        urlField.text = webView.request?.URL.absoluteString
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
+        var message = "That page could not be loaded. " + error.localizedDescription
+        let alert = UIAlertController(title: "Could not load URL", message: message, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "That's Sad", style: .Default, handler: nil)
+        alert.addAction(okAction)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
